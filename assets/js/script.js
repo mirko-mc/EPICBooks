a0480be12d09475041facb13b9b42b0fb12480b6
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-06-03 18:00:57 +0200

    WIP - exercise complete with bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 let books = null;
 let main = document.querySelector("main");
+let count = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   getData().then((books) => {
-    //   console.log("DOMLOADED => books\n", books);
     books.forEach((book) => {
-      // console.log("DOMLOADED => book\n", book);
       createCards(book.id, book.image, book.title, book.price);
     });
   });
@@ -13,21 +12,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function createCards(id, image, title, price) {
   main.innerHTML += `
-  <div class="col-3">
+  <div class="col-3 p-1">
     <div class="card">
-      <img src="${image}" class="card-img-top" alt="${title}">
+      <img src="${image}" class="card-img-top img-fluid" alt="${title}">
       <div class="card-body">
         <h5 class="card-title text-truncate">${title}</h5>
         <p class="card-text">${price} €</p>
       <div class="d-flex justify-content-around">
         <button class="btn btn-primary" onclick="addToCart(this)" id="asin${id}">Aggiungi al carrello</button>
-        <button class="btn btn-primary">Salta prodotto</button>
       </div>
     </div>
   </div>
   `;
 }
-let count = 0;
 function addToCart(cart) {
   const TRASH = `<svg
     xmlns="http://www.w3.org/2000/svg"
@@ -41,16 +38,14 @@ function addToCart(cart) {
     <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
   </svg>`;
   const ASIN = cart.id.slice(4);
-  // console.log("ID\n", ASIN);
-  console.log("ID\n", cart);
   const SUPERCONTAINER = document.querySelector("#superContainer .row");
   if (!document.getElementById("cart")) {
     SUPERCONTAINER.innerHTML += `
     <aside id="cart" class="col-2 gx-3 gy-4">
-      <div class="col-12">
+      <div class="col-12 d-flex flex-column align-items-center">
       <h3>CARRELLO</h3>
       <span>LIBRI AGGIUNTI : </span>
-      <button class="btn btn-primary" onclick="clearCart()">Svuota carrello</button>
+      <button class="btn btn-primary mb-2" onclick="clearCart()">Svuota carrello</button>
       </div>
     </aside>`;
   }
@@ -58,16 +53,14 @@ function addToCart(cart) {
   const ADDED = document.querySelector(`aside .${cart.id}`);
   const LIBRIAGGIUNTI = document.querySelector("aside span");
   const ASIDE = document.querySelector("aside .col-12");
-  LIBRIAGGIUNTI.textContent = `LIBRI AGGIUNTI : ${count++}`;
-  // console.log(!ADDED);
+  LIBRIAGGIUNTI.textContent = `LIBRI AGGIUNTI : ${++count}`;
   getData().then((BOOKS) => {
-    // console.log("ORA SONO QUI");
     if (!ADDED) {
       for (const BOOK of BOOKS) {
         if (BOOK.id === ASIN) {
           ASIDE.innerHTML += `
-          <div class="card mb-3">
-            <div class="card-body d-flex flex-wrap justify-content-between">
+          <div class="card mb-1 w-100">
+            <div class="card-body d-flex flex-wrap justify-content-between p-2">
               <h6 class="title text-truncate w-100">${BOOK.title}</h6>
               <span class="card-text">${BOOK.price}</span>
               <a href="#" class="btn btn-primary" onclick=deleteBook(this,${ASIN})>${TRASH}</a
@@ -75,7 +68,6 @@ function addToCart(cart) {
           </div>
           `;
           cart.classList.toggle("disabled");
-          // console.log(cart.classList);
         }
       }
     } else alert("Libro già presente nel carrello.");
@@ -102,18 +94,17 @@ async function getData() {
     return [];
   }
 }
-// SUGGERIMENTO: usa .filter()
 function searchBook() {
   const INPUT = document.getElementById("searchBook").value.toLowerCase();
   main.innerHTML = "";
   console.clear();
-  /* QUESTO E' IL CODICE PER CERCARE DA UNA DATA LETTERA IN POI ANZICHE' DALLA PRIMA
-  console.log(Array.from(INPUT).length);
+  /* QUESTA E' L'ISTRUZIONE SU CUI BASARSI PER CERCARE DA UNA N LETTERA IN POI ANZICHE' DALLA PRIMA
+  Array.from(INPUT).length;
   */
   getData().then((books) => {
-    books.forEach((book, i) => {
+    books.forEach((book) => {
       book.title.toLowerCase().includes(INPUT)
-        ? createCards(book.image, book.title, book.price, i)
+        ? createCards(book.id, book.image, book.title, book.price)
         : "";
     });
   });
@@ -125,11 +116,14 @@ function clearCart() {
   for (const BUTTON of BUTTONS) {
     BUTTON.classList.toggle("disabled");
   }
+  count = 0;
 }
 
-function deleteBook(trash,asin) {
+function deleteBook(trash, asin) {
   console.log(asin);
   console.log(document.getElementById(`asin${asin}`).classList);
   document.getElementById(`asin${asin}`).classList.toggle("disabled");
   trash.parentNode.parentNode.remove();
+  const LIBRIRIMOSSI = document.querySelector("aside span");
+  LIBRIRIMOSSI.textContent = `LIBRI AGGIUNTI : ${--count}`;
 }
